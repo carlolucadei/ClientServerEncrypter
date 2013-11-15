96abc6ae055507ce3ec3e491114d92a8a883cd5c
carlo.lucadei <carlo.lucadei@speechvillage.com>
2013-11-15 11:15:59 +0100

    added git ignore and base engine

---
+++ src/ClientServerEncrypter/Entities/EncrypterDescrypter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientServerEncrypter.Entities
+{
+    public class EncrypterDescrypter
+    {
+    }
+}
