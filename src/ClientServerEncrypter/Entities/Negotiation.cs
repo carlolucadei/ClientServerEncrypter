ebb112859ef8aafd2d5b4fbe57251e55b8f4e67d
carlo.lucadei <carlo.lucadei@speechvillage.com>
2013-11-15 14:17:01 +0100

    added Negotiation class

---
+++ src/ClientServerEncrypter/Entities/Negotiation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientServerEncrypter.Entities
+{
+    public class Negotiation : EncrypterDescrypter
+    {
+        public byte[] SendPublicKey()
+        {
+            return base.EncryptPublicKey();
+        }
+        public void GetPublicKey(byte[] key, byte[] iv){
+            this.DecryptPublicKey(key, iv);
+        }
+
+        public string SendMessage(string message)
+        {
+            if (!this.CheckExternalKey())
+                throw new InvalidOperationException("Cannot send message with a not valid public key");
+            return this.EncryptMessage(message);
+        }
+
+        public string GetMessage(string message)
+        {
+            return this.DecryptMessage(message);
+        }
+    }
+}
